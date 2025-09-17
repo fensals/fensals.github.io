95774e663b4f9232c8e83ef98f2f60861a588aff
Femi Aleyemi <fensalsdx@gmail.com>
2025-09-17 17:14:23 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("a.nav-link");
+  links.forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+});
